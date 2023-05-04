0b8e78e77fd64e1d3c361029b4a6d597ba47d6f9
UmangKhandelwal16 <umangkhandelwal1604@gmail.com>
2023-05-04 11:57:14 +0530

    Navbar updated

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from 'react-router-dom';
+import './NavBar.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+
+const NavBar = () => {
+
+
+  return (
+    <Router>
+    <div>
+        <Navbar fixed="top" bg="dark" variant={"dark"} expand="lg">
+        <Container>
+          <Navbar.Brand as={NavLink} to={"/"}>GYM</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+            <NavDropdown title="HOME" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#pricing">Checkout Our Prices </NavDropdown.Item>
+                <NavDropdown.Item href="#programs">Training Programs </NavDropdown.Item>
+                <NavDropdown.Item href="#trainer"> Meet Your Trainers</NavDropdown.Item>
+                <NavDropdown.Item href="#blogs">Blogs</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link as={NavLink} to="/about">ABOUT US </Nav.Link>
+              <Nav.Link as={NavLink} to="/programs">PROGRAMS</Nav.Link>
+              <Nav.Link as={NavLink} to="/blogs">BLOGS</Nav.Link>
+              <Nav.Link as={NavLink} to="/pricing">PRICING</Nav.Link>
+            </Nav>
+            <Button    variant = "danger" className = "joinBtn">Join Us</Button>   
+            </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+    </Router>
+  )
+}
+
+export default NavBar
