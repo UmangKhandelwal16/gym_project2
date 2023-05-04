a3d028cf48e8215956684f46f5852a1dd5e18c73
UmangKhandelwal16 <umangkhandelwal1604@gmail.com>
2023-05-04 13:23:24 +0530

    Navbar created and modified

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Corousel from '../../Components/Corousel/Corousel';
+import GymAboutUs from '../../Components/AboutUs/AboutUs';
+import NavBar from '../../Components/NavBar/NavBar';
+
+function Home() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Corousel/>
+      <GymAboutUs/>
+    </div>
+  );
+}
+
+export default Home;
