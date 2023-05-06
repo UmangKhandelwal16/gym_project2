8a9a3bbd701f436090152fc741cdf80b603592bc
Madhvi Malviya <madhvimalviya1@gmail.com>
2023-05-06 19:04:16 +0530

    final button import changes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,20 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Corousel from '../../Components/Corousel/Corousel';
-import Pricing from '../../Components/PricingSection/Pricing';
-import Programs from '../../Components/TrainingPrograms/Programs';
-import About from '../../Components/AboutUs/AboutUs'
-import Trainings from '../../Components/Blogs/Trainings';
-import Register from '../../Components/Register/Register';
+// import Pricing from '../../Components/PricingSection/Pricing';
+// import Programs from '../../Components/TrainingPrograms/Programs';
+// import About from '../../Components/AboutUs/AboutUs'
+// import Trainings from '../../Components/Blogs/Trainings';
+// import Register from '../../Components/Register/Register';
 
 function Home() {
   return (
     <div className="App">
       <Corousel/>
-      <About/>
+      {/* <About/>
       <Programs/>
       <Pricing/>
       <Trainings/>
-      <Register/>
+      <Register/> */}
     </div>
   );
 }
