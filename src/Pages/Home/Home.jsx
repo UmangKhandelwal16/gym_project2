f4a3f7a3fcc22bec428323a76f577a22d76d63e1
Madhvi Malviya <madhvimalviya1@gmail.com>
2023-05-06 19:21:46 +0530

    final  updated

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,20 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Corousel from '../../Components/Corousel/Corousel';
-// import Pricing from '../../Components/PricingSection/Pricing';
-// import Programs from '../../Components/TrainingPrograms/Programs';
-// import About from '../../Components/AboutUs/AboutUs'
-// import Trainings from '../../Components/Blogs/Trainings';
-// import Register from '../../Components/Register/Register';
+import Pricing from '../../Components/PricingSection/Pricing';
+import Programs from '../../Components/TrainingPrograms/Programs';
+
+import About from '../../Components/AboutUs/AboutUs'
+import Trainings from '../../Components/Blogs/Trainings';
+import Register from '../../Components/Register/Register';
 
 function Home() {
   return (
     <div className="App">
       <Corousel/>
-      {/* <About/>
       <Programs/>
       <Pricing/>
+      <About/>
       <Trainings/>
-      <Register/> */}
+      <Register/>
     </div>
   );
 }
